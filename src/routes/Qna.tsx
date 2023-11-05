c2adf8f7617d916f54459bff9280f84c754307e6
dlrhdns75177 <sararhdns7517@naver.com>
2023-11-05 19:35:03 +0900

    feat: 문의하기 페이지, 틀 수정2

--- src/routes/Qna.tsx
+++ src/routes/Qna.tsx
@@ -1,11 +1,19 @@
 import { useState } from "react";
 import styled from "styled-components";
 
+interface FileInfo {
+  id: string;
+  file: File;
+}
+function generateId() {
+  return new Date().getTime().toString();
+}
+
 function Qna() {
   const [emailValue, setEmail] = useState("");
   const [titleValue, setTitle] = useState("");
   const [contentValue, setContent] = useState("");
-  const [file, setFile] = useState<File | null>(null);
+  const [files, setFiles] = useState<FileInfo[]>([]);
   const EmailInput = (event: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(event.target.value);
     console.log(event.target.value);
@@ -19,16 +27,26 @@ function Qna() {
   };
 
   const fileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
-    // 추가
     if (event.target.files) {
-      setFile(event.target.files[0]);
+      const selectedFiles = Array.from(event.target.files);
+      const newFiles: FileInfo[] = selectedFiles.map((file) => ({
+        id: generateId(),
+        file: file,
+      }));
+      setFiles((prevFiles) => [...prevFiles, ...newFiles]);
     }
+  };
+  const RemoveFile = (id: string) => {
+    const updatedFiles = files.filter((file) => file.id !== id);
+    setFiles(updatedFiles);
   };
   return (
     <Container>
-      <QnaTitle>
-        <h2>문의주세용가리치킨:)</h2>
-      </QnaTitle>
+      <QnaTtilePosition>
+        <QnaTitle>
+          <h2>문의주세용가리치킨:)</h2>
+        </QnaTitle>
+      </QnaTtilePosition>
       <QnaContainer>
         <QnaMain>
           <QnaCategory>
@@ -58,14 +76,28 @@ function Qna() {
             />
           </TitleDiv>
           <AttachFile>
-            <FileLabel htmlFor="upload-button">파일 선택</FileLabel>
-            <input
-              id="upload-button"
-              type="file"
-              style={{ display: "none" }}
-              onChange={fileUpload}
-            />
-            {file && <div>{file.name}</div>}
+            <FileContainer>
+              <AttachFileDiv>
+                <FileLabel htmlFor="upload-button">파일 선택</FileLabel>
+                <input
+                  id="upload-button"
+                  type="file"
+                  style={{ display: "none" }}
+                  multiple
+                  onChange={fileUpload}
+                />
+              </AttachFileDiv>
+              <FileBox>
+                {files.map((file) => (
+                  <FileName key={file.id}>
+                    {file.file.name}
+                    <FileDelet onClick={() => RemoveFile(file.id)}>
+                      취소
+                    </FileDelet>
+                  </FileName>
+                ))}
+              </FileBox>
+            </FileContainer>
           </AttachFile>
           <ContentDiv>
             <h4>Message</h4>
@@ -78,46 +110,63 @@ function Qna() {
         </QnaMain>
       </QnaContainer>
       <div>말풍선 만들 거</div>
-      <QnaCharacter>
-        모람이가 말하고 있는것처럼 말풍선으로 만들 예정
-        <img src="./assets/profileimage.jpg" />
-      </QnaCharacter>
+      <QnaCharacterPosition>
+        <QnaCharacter>
+          모람이가 말하고 있는것처럼 말풍선으로 만들 예정
+          <img src="./assets/profileimage.jpg" />
+        </QnaCharacter>
+      </QnaCharacterPosition>
     </Container>
   );
 }
 
 const Container = styled.div`
   height: 100%;
-  width: 60%;
+  width: 90%;
+  max-width: 800px;
   margin: auto;
   display: flex;
   flex-direction: column;
+  align-items: center;
 `;
 const QnaContainer = styled.div`
-  max-width: 800px;
   width: 100%;
   margin: 1rem 4rem;
-  padding: 3rem 6rem;
+  padding: 3rem 12%;
   border: solid 1px gray;
   border-radius: 15px;
 `;
 const QnaCategory = styled.div`
   display: flex;
-  gap: 0.5rem;
-  padding: 0 2rem;
+  gap: 1%;
+  padding: 0 5%;
   justify-content: space-between;
   & button {
-    width: 100px;
+    width: 110px;
     height: 40px;
-    font-size: 15px;
+    font-size: 14px;
     background-color: transparent;
     outline: 0;
     border: solid 1px gray;
     border-radius: 15px;
   }
+  @media screen and (max-width: 1000px) {
+    & button {
+      font-size: 12px;
+    }
+  }
+  @media screen and (max-width: 780px) {
+    & button {
+      font-size: 10px;
+    }
+  }
+`;
+const QnaTtilePosition = styled.div`
+  width: 100%;
 `;
 const QnaTitle = styled.div`
   display: flex;
+  justify-content: flex-start;
   margin-top: 1rem;
   margin-bottom: 2rem;
   font-size: 1.5rem;
@@ -139,7 +188,7 @@ const EmailDiv = styled.div`
   display: flex;
   width: 100%;
   & h4 {
-    margin-right: 1.5rem;
+    margin-right: 3.5rem;
   }
   & ${Input} {
   }
@@ -148,7 +197,7 @@ const TitleDiv = styled.div`
   display: flex;
   width: 100%;
   & h4 {
-    margin-right: 2.1rem;
+    margin-right: 4.3rem;
   }
 `;
 const ContentDiv = styled.div`
@@ -160,22 +209,55 @@ const ContentTextarea = styled.textarea`
   height: 300px;
   text-align: left;
 `;
+
 const AttachFile = styled.div`
   display: flex;
+  flex-direction: columns;
+`;
+const FileContainer = styled.div`
+  display: flex;
+  width: 100%;
+`;
+const AttachFileDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const FileBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  border: 1px solid gray;
+  border-radius: 4px;
+  width: 100%;
+`;
+const FileName = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const FileDelet = styled.button`
+  margin-left: 10px;
+  background-color: transparent;
+  outline: 0;
+  border: 0;
 `;
 const FileLabel = styled.label`
   display: inline-block;
-  padding: 5px 10px;
+  padding: 3px 2px;
+  margin-right: 2.2rem;
   background-color: #f2f2f2;
   border: 1px solid #ccc;
   border-radius: 4px;
   cursor: pointer;
 `;
-
+const QnaCharacterPosition = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: flex-end;
+`;
 const QnaCharacter = styled.div`
   display: flex;
+  width: 100%;
   justify-content: flex-end;
-  & img {8
+  & img {
   }
 `;
 export default Qna;
