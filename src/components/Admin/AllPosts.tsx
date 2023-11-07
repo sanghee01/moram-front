d6ba604967db27410b6f4378f68cd34bb8b1f49b
sanghee01 <sang6299@naver.com>
2023-11-07 11:30:19 +0900

    feat: 모든글 화면에 불러옴

--- src/components/Admin/AllPosts.tsx
+++ src/components/Admin/AllPosts.tsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { styled } from "styled-components";
 import { useEffect, useState } from "react";
+import { Title, Table, Row } from "../../styles/TableStyles";
 
 function AllPosts() {
   const [allPostsData, SetAllPostsData] = useState<null | any>(null);
@@ -21,7 +22,55 @@ function AllPosts() {
       alert(error.response.data);
     }
   };
-  return <>hi</>;
+  return (
+    <Container>
+      <Title>작성된 글 목록</Title>
+      {loading ? (
+        <div>loading...</div>
+      ) : (
+        <>
+          <span>총합 : {allPostsData.length}</span>
+          <Table>
+            <thead>
+              <tr>
+                <th>ID</th>
+                {/* <th>닉네임</th> */}
+                <th>제목</th>
+                <th>작성시간</th>
+                <th>태그</th>
+                <th>카테고리</th>
+              </tr>
+            </thead>
+            <tbody>
+              {allPostsData.map((post: any) => {
+                return (
+                  <Row key={post.id}>
+                    <td>{post.id}</td>
+                    {/* <td>{post.nickname}</td> */}
+                    <td>{post.title}</td>
+                    <td>{post.writeTime}</td>
+                    <td>{post.tag}</td>
+                    <td>{post.category}</td>
+                  </Row>
+                );
+              })}
+            </tbody>
+          </Table>
+        </>
+      )}
+    </Container>
+  );
 }
 
 export default AllPosts;
+
+const Container = styled.div`
+  width: 100%;
+  margin: auto;
+  margin-top: 50px;
+  height: 100vh;
+  @media screen and (max-width: 800px) {
+    width: 90%;
+    font-size: 0.8rem;
+  }
+`;
