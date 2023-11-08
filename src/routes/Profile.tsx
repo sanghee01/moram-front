224744658278daabfc1342fc9df2c06d9a8e8572
DESKTOP-LR64C9E\USER <sararhdns7517@naver.com>
2023-11-09 03:33:36 +0900

    feat: 프로필 페이지 버튼 클릭 이벤트 구현

--- src/routes/Profile.tsx
+++ src/routes/Profile.tsx
@@ -1,9 +1,10 @@
-import { styled } from "styled-components";
+import  styled  from "styled-components";
 import { IoSettingsOutline } from "react-icons/io5";
 import { PiListBold } from "react-icons/pi";
 import { AiOutlinePicture } from "react-icons/ai";
 import { BiText } from "react-icons/bi";
-import React, { useState } from "react";
+import React, { useState ,useEffect } from "react";
+import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 interface ChooseContentProps {
@@ -12,6 +13,7 @@ interface ChooseContentProps {
   title: string;
   date: string;
   content: string;
+  selectIcon: "all" | "picture" | "letter";
 }
 
 function ChooseContent({
@@ -20,20 +22,36 @@ function ChooseContent({
   title,
   date,
   content,
+  selectIcon
 }: ChooseContentProps) {
   return (
     <Content>
-      {}
-      <img src={src} alt="이미지" />
-      <section>
-        <div>
-          <span>
-            [{category}] {title}
-          </span>
-          <span>{date}</span>
-        </div>
-        <p>{content}</p>
-      </section>
+      {selectIcon === "all" && (
+        <>
+          <img src={src} alt="이미지" />
+          <section>
+            <div>
+              <span>
+                [{category}] {title}
+              </span>
+              <span>2023-11-08 15:30:53</span>
+            </div>
+            <p>{content}</p>
+          </section>
+        </>
+      )}
+      {selectIcon === "picture" && <img src={src} alt="이미지" />}
+      {selectIcon === "letter" && (
+        <section>
+          <div>
+            <span>
+              [{category}] {title}
+            </span>
+            <span>2023-11-08 15:30:53</span>
+          </div>
+          <p>{content}</p>
+        </section>
+      )}
     </Content>
   );
 }
@@ -42,36 +60,77 @@ function Profile() {
   const [selectIcon, setSelectIcon] = useState<"all" | "picture" | "letter">(
     "all"
   );
+  const [selectedTab, setSelectedTab] = useState<"posts" | "comments">("posts");
 
+  //백엔드에서 불러올때
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://your-backend-url/posts")
+      .then((response) => {
+        setPosts(response.data); // 받아온 데이터를 state에 저장
+      })
+      .catch((error) => {
+        console.error("There was an error!", error);
+      });
+  }, []);
+
+  //백엔드 아직 불러오지x
   const onClickIcon = (iconType: "all" | "picture" | "letter") => {
     setSelectIcon(iconType);
   };
 
   const navigate = useNavigate();
 
+  const onClickWritePost = () => {
+    setSelectedTab("posts");
+    console.log("작성한 글 버튼이 클릭되었습니다.");
+  };
+  const onClickWriteComment = () => {
+    setSelectedTab("comments");
+    console.log("댓글 단 글 버튼이 클릭되었습니다.");
+  };
+
+  const onClickAllIcon = () => {
+    setSelectIcon("all");
+  };
+
+  const onClickPictureIcon = () => {
+    setSelectIcon("picture");
+  };
+
+  const onClickLetterIcon = () => {
+    setSelectIcon("letter");
+  };
+
+
   return (
-    <Container>
+    <MainContainer>
       <ProfileMain>
+        <ProfileContainer>
           <img src="./assets/profileimage.jpg" />
-          <ProfileInpromation>
-          <InformationBox>
-           <ProfileContent>
-             <SettingIcon onClick={() => navigate("/profile-edit")}>
-               <IoSettingsOutline size="20" />
-             </SettingIcon>
-             <h1>쿼카맹구</h1>
-             <h4>dlrhdns0000@naver.com</h4>
-           </ProfileContent>
-           <ProfileWrite>
-             <button>작성한 글</button>
-             <button>댓글 단 글</button>
-           </ProfileWrite>
-           </InformationBox>
-          </ProfileInpromation>
+          <Container>
+            <ProfileInpromation>
+              <InformationBox>
+                <ProfileContent>
+                  <SettingIcon onClick={() => navigate("/profile-edit")}>
+                    <IoSettingsOutline size="20" />
+                  </SettingIcon>
+                  <h1>쿼카맹구</h1>
+                  <h4>dlrhdns0000@naver.com</h4>
+                </ProfileContent>
+                <ProfileWrite>
+                  <button onClick={onClickWritePost}>작성한 글</button>
+                  <button onClick={onClickWriteComment}>댓글 단 글</button>
+                </ProfileWrite>
+              </InformationBox>
+            </ProfileInpromation>
+          </Container>
+        </ProfileContainer>
       </ProfileMain>
       <ChooseIcon>
         <ChooseAllIcon
-          onClick={() => onClickIcon("all")}
+          onClick={onClickAllIcon}
           style={
             selectIcon === "all" ? { borderBottom: "2px solid #343434" } : {}
           }
@@ -82,7 +141,7 @@ function Profile() {
           />
         </ChooseAllIcon>
         <ChoosePictureIcon
-          onClick={() => onClickIcon("picture")}
+          onClick={onClickPictureIcon}
           style={
             selectIcon === "picture"
               ? { borderBottom: "2px solid #343434" }
@@ -95,7 +154,7 @@ function Profile() {
           />
         </ChoosePictureIcon>
         <ChooseLetterIcon
-          onClick={() => onClickIcon("letter")}
+          onClick={onClickLetterIcon}
           style={
             selectIcon === "letter" ? { borderBottom: "2px solid #343434" } : {}
           }
@@ -106,48 +165,84 @@ function Profile() {
           />
         </ChooseLetterIcon>
       </ChooseIcon>
-      <ChooseContentBox>
+      
+      {selectedTab === "posts" && (
+<ChooseContentBox selectIcon={selectIcon}>
+       {/*  백엔드에서 데이터 불러올때
+      {posts.map((post, index) => (
+      <ChooseContent
+        key={index}
+        src={post.imageSrc}
+        category={post.category}
+        title={post.title}
+        date={post.date}
+        content={post.content}
+        selectIcon={selectIcon}
+      />
+    ))} */}
         <ChooseContent
           src="https://react.dev/images/home/conf2021/cover.svg"
           category="스터디"
           title="React 스터디 구합니다"
           date="2023.10.10."
-          content="React 기초부터 같이 학습하실 분 구합니다. 어느정
-    프로젝트도 할 예정입니다."
+          content="React 기초부터 같이 학습하실 분 구합니다. 어느정 프로젝트도 할 예정입니다."
+          selectIcon={selectIcon}
         />
         <ChooseContent
           src="https://react.dev/images/home/conf2021/cover.svg"
           category="스터디"
           title="React 스터디 구합니다"
           date="2023.10.10."
-          content="React 기초부터 같이 학습하실 분 구합니다. 어느정
-로젝트도 할 예정입니다."
+          content="React 기초부터 같이 학습하실 분 구합니다. 어느정 로젝트도 할 예정입니다."
+          selectIcon={selectIcon}
         />
         <ChooseContent
           src="https://react.dev/images/home/conf2021/cover.svg"
           category="스터디"
           title="React 스터디 구합니다"
           date="2023.10.10."
-          content="React 기초부터 같이 학습하실 분 구합니다. 어느정
-로젝트도 할 예정입니다."
+          content="React 기초부터 같이 학습하실 분 구합니다. 어느정 로젝트도 할 예정입니다."
+          selectIcon={selectIcon}
         />
+        <ChooseContent
+          src="https://react.dev/images/home/conf2021/cover.svg"
+          category="스터디"
+          title="React 스터디 구합니다"
+          date="2023.10.10."
+          content="React 기초부터 같이 학습하실 분 구합니다. 어느정 로젝트도 할 예정입니다."
+          selectIcon={selectIcon}
+        />
+        <ChooseContent
+          src="https://react.dev/images/home/conf2021/cover.svg"
+          category="스터디"
+          title="React 스터디 구합니다"
+          date="2023.10.10."
+          content="React 기초부터 같이 학습하실 분 구합니다. 어느정 로젝트도 할 예정입니다."
+          selectIcon={selectIcon}
+        />
+        
       </ChooseContentBox>
-    </Container>
+      )}
+      
+      {selectedTab === "comments" && <p>여기는 내가 댓글 단 글이 보여요요용</p>}
+    </MainContainer>
   );
 }
 
-const Container = styled.div`
-  width: 60%;
-  justify-content: center;
-  height: 100vh;
-  margin: auto;
+
+const MainContainer = styled.div`
+display: flex;
+flex-direction: column;
+width: 58%;
+justify-content: center;
+margin: auto;
+margin-top: 1.5rem;
 `; 
 
 const ProfileMain = styled.div`
   display: flex;
   width: 100%;
-  align-items: center;
-  justify-content: space-between;
+  justify-content: center;
   margin: 1.5rem 0;
   & section {
     display: flex;
@@ -172,24 +267,58 @@ const ProfileMain = styled.div`
   }
 `;
 
+const ProfileContainer = styled.div`
+display: flex;
+width: 100%;
+`; 
+const Container = styled.div`
+  display: flex;
+  width: 100%;
+`;
 const ProfileInpromation = styled.div`
-  padding: 0 1rem;
+  padding-left: 1rem;
   width: 100%;
   display: flex;
   justify-content: center;
-  @media screen and (max-width: 800px) {
+  @media screen and (max-width: 1000px) {
     & h1 {
-      font-size: 28px;
+      font-size: 30px;
     }
     & h4 {
-      font-size: 13px;
+      font-size: 16px;
+    }
+  }
+  @media screen and (max-width: 800px) {
+    & h1 {
+      font-size: 25px;
+    }
+    & h4 {
+      font-size: 11px;
+    }
+  }
+  @media screen and (max-width: 600px) {
+    & h1 {
+      font-size: 22px;
+    }
+    & h4 {
+      font-size: 9px;
     }
   }
 `;
 const ProfileContent = styled.div`
 display: flex;
 flex-direction: column;
+justify-content: center;
 align-items: flex-start;
+width: 300px;
+ @media screen and (max-width: 600px) {
+    & h1 {
+      font-size: 22px;
+    }
+    & h4 {
+      font-size: 9px;
+    }
+  }
 `;
 
 const InformationBox = styled.div`
@@ -199,6 +328,7 @@ display: flex;
   gap: 0.8rem;
   @media screen and (max-width: 1400px) {
     flex-direction: column;
+    justify-content: center; 
   }
 `;
 const SettingIcon = styled.button`
@@ -215,19 +345,32 @@ const SettingIcon = styled.button`
 
 const ProfileWrite = styled.div`
   display: flex;
-  width: 70%;
+  width: 75%;
   align-items: center;
   gap: 0.5rem;
   & button {
   background-color: transparent;
+  font-size: 15px;
   outline: 0;
   border: 0;
   background-color: #e6e6e6d6;
-  min-width: 200px;
-  width: 50%;
+  width: 100%;
   height: 35px;
   border-radius: 8px;
   gap: 0.2rem;
+  }
+  @media screen and (max-width: 1000px) {
+    & button {
+      width: 160px;
+      height: 30px;
+    }
+  }
+  @media screen and (max-width: 800px) {
+    & button {
+      width: 130px;
+      height: 30px;
+      font-size: 13px;
+    }
   }
 `;
 
@@ -252,6 +395,7 @@ const ChoosePictureIcon = styled.button`
   outline: 0;
   border: 0;
   width: 346px;
+
 `;
 const ChooseLetterIcon = styled.button`
   background-color: transparent;
@@ -265,28 +409,42 @@ const Content = styled.div`
   gap: 20px;
   margin-top: 10px;
   & img {
-    width: 200px;
-    height: 200px;
+    width: 217px;
+    height: 217px;
     object-fit: cover;
     border-radius: 8px;
   }
-  @media screen and (max-width: 1000px) {
+  @media screen and (max-width: 1150px) {
     & img {
-      width: 70px;
-      height: 70px;
+      width: 125px;
+      height: 125px;
+    }
+  }
+  @media screen and (max-width: 800px) {
+    & img {
+      width: 100px;
+      height: 100px;
     }
   }
   @media screen and (max-width: 600px) {
     & img {
-      width: 50px;
-      height: 50px;
+      width: 60px;
+      height: 60px;
     }
   }
 `;
+type BoxProps = {
+  selectIcon: "all" | "picture" | "letter";
+};
 
-const ChooseContentBox = styled.div`
+const ChooseContentBox = styled.div<BoxProps>`
   display: flex;
-  flex-direction: column;
+  flex-direction: ${props => (props.selectIcon === "picture" ? "row" : "column")};
+  flex-wrap: ${props => (props.selectIcon === "picture" ? "wrap" : "nowrap")};
+  justify-content: start;
+  gap:0.2rem;
 `;
+
+
 
 export default Profile;
