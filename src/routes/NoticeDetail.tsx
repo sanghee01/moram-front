336a7d9393214fc00d7f1f39c131ab49bb8015f2
sanghee01 <sang6299@naver.com>
2023-11-10 02:25:12 +0900

    feat: 클릭한 공지글 디테일,공지글 삭제

--- src/routes/NoticeDetail.tsx
+++ src/routes/NoticeDetail.tsx
@@ -1,5 +1,92 @@
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import styled from "styled-components";
+import { useEffect, useState } from "react";
+import { handleDateChange } from "../dateChange";
+import { SmallBtn } from "../styles/ButtonStyles";
+import { useNavigate } from "react-router-dom";
+
 function NoticeDetail() {
-  return <>hi</>;
+  const params = useParams();
+  const noticeId = params.id;
+  const [noticeContent, setNoticeContent] = useState<any>();
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    getNoticePost();
+  }, []);
+
+  const getNoticePost = async () => {
+    try {
+      const response = await axios.get(
+        `${process.env.REACT_APP_APIADDRESS}/notice/${noticeId}`
+      );
+      setNoticeContent(response.data.content);
+      console.log("noticeContent: ", response.data.content);
+      setLoading(false);
+    } catch (error: any) {
+      alert(error?.response?.data?.message || "알 수 없는 에러 발생.");
+    }
+  };
+
+  const deleteNoticePost = async () => {
+    try {
+      const response = await axios.delete(
+        `${process.env.REACT_APP_APIADDRESS}/notice/${noticeId}`
+      );
+      alert(response.data.message);
+      navigate("/notice");
+    } catch (error: any) {
+      alert(error?.response?.data?.message || "알 수 없는 에러 발생.");
+    }
+  };
+
+  return (
+    <>
+      {loading ? (
+        <div>loading...</div>
+      ) : (
+        <Container>
+          <h2>{noticeContent.title}</h2>
+          <h4>
+            {noticeContent.nickname} |{" "}
+            {handleDateChange(noticeContent.writeTime)}
+            <SmallBtn
+              $padding="4px 10px"
+              $margin="5px"
+              $background="tomato"
+              $backgroundHover="red"
+              $color="white"
+              onClick={() => deleteNoticePost()}
+            >
+              글 삭제
+            </SmallBtn>
+            <SmallBtn
+              $padding="4px 10px"
+              $margin="5px"
+              $background="skyblue"
+              $backgroundHover="lightblue"
+              $color="white"
+            >
+              글 수정
+            </SmallBtn>
+          </h4>
+          <hr />
+          <div>{noticeContent.content}</div>
+        </Container>
+      )}
+    </>
+  );
 }
+
+const Container = styled.div`
+  width: calc(100% - 30px);
+  max-width: 800px;
+  padding: 15px;
+  margin: 15px auto;
+  background-color: whitesmoke;
+  border-radius: 15px;
+`;
 
 export default NoticeDetail;
