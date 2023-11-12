72bace6d37e9f597d734c0953244df1d4377a397
sanghee01 <sang6299@naver.com>
2023-11-12 20:14:00 +0900

    fix: 프로필 디자인, 기타 버그 수정

--- src/components/Profile/MyPost.tsx
+++ src/components/Profile/MyPost.tsx
@@ -1,54 +1,62 @@
 import { styled } from "styled-components";
 import { useNavigate } from "react-router-dom";
+import { Tag, Category } from "../../styles/Tag_CatagoryStyles";
+
 interface PostContentProps {
-    id: number;
-    img?: string;
-    category: string;
-    title: string;
-    date: string;
-    content: string;
-    tag: string;
-  }
+  id: number;
+  nickname: string;
+  hitCount: number;
+  likesCount: number;
+  commentCount: number;
+  img?: string;
+  category: string;
+  title: string;
+  date: string;
+  content: string;
+  tag: string;
+}
 
-  function MyPost({
-    id,
-    img,
-    category,
-    title,
-    date,
-    content,
-    tag,
-  }: PostContentProps) {
-    const navigate = useNavigate();
-  
-    return (
-      <Container
-        onClick={() => {
-            navigate(`/community/${id}`);
-        }}
-      >
-        {img ? (
-          <img src={img} alt="이미지" />
-        ) : (
-          <img src="https://i.ibb.co/2Y3sQX2/noImage.png" alt="기본이미지" />
-        )}
-        <div>
-          <Title>
-            <div>
-              <span>
-                [{tag}] {title}
-              </span>
-            </div>
-            <Time> {date}</Time>
-          </Title>
-          <p>{content}</p>
-        </div>
-      </Container>
-    );
-  }
+function MyPost({
+  id,
+  nickname,
+  hitCount,
+  likesCount,
+  commentCount,
+  img,
+  category,
+  title,
+  date,
+  content,
+  tag,
+}: PostContentProps) {
+  const navigate = useNavigate();
 
-  
-  const Container = styled.div`
+  return (
+    <Container
+      onClick={() => {
+        navigate(`/community/${id}`);
+      }}
+    >
+      {img ? (
+        <img src={img} alt="이미지" />
+      ) : (
+        <img src="https://i.ibb.co/2Y3sQX2/noImage.png" alt="기본이미지" />
+      )}
+      <div>
+        <Title>
+          <Tag>{tag}</Tag>
+          <Category>{category}</Category>
+          <div>{title}</div>
+          <Info>
+            | {nickname} | ❤️{likesCount} 👀{hitCount} 💬{commentCount} | {date}
+          </Info>
+        </Title>
+        <ContentText>{content.split("<br/>").join(" ")}</ContentText>
+      </div>
+    </Container>
+  );
+}
+const Container = styled.div`
   display: flex;
   gap: 20px;
   border-bottom: 1px solid rgba(0, 0, 0, 0.3);
@@ -69,47 +77,52 @@ interface PostContentProps {
     margin: 5px 0;
   }
   & div {
-    width: 100%;
   }
 
-@media screen and (max-width: 1000px) {
-  & img {
-    width: 70px;
-    height: 70px;
+  @media screen and (max-width: 1000px) {
+    & img {
+      width: 70px;
+      height: 70px;
+    }
   }
-}
-@media screen and (max-width: 600px) {
-  & img {
-    width: 50px;
-    height: 50px;
+  @media screen and (max-width: 600px) {
+    & img {
+      width: 50px;
+      height: 50px;
+    }
   }
-}
 `;
 
-const Title = styled.header`
-display: flex;
-justify-content: space-between;
-align-items: center;
-font-weight: bold;
+const Title = styled.div`
+  display: flex;
+  align-items: center;
+  font-weight: bold;
+  gap: 5px;
 
-& div {
-  width: 80%;
-}
-
-@media screen and (max-width: 1000px) {
-  flex-direction: column;
-  align-items: start;
-  gap: 0;
-  & div {
-    width: inherit;
+  @media screen and (max-width: 1000px) {
+    flex-direction: column;
+    align-items: start;
+    gap: 0;
+    & div {
+      width: inherit;
+    }
   }
-}
 `;
 
-const Time = styled.span`
-opacity: 0.5;
-font-weight: 500;
-font-size: 0.9rem;
+const Info = styled.span`
+  font-weight: 500;
+  font-size: 0.9rem;
 `;
-  export default MyPost;
-  
+
+const ContentText = styled.div`
+  display: flex;
+  white-space: pre-wrap;
+  margin-top: 10px;
+  display: -webkit-box;
+  overflow: hidden;
+  line-height: 1.7;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+`;
+
+export default MyPost;
