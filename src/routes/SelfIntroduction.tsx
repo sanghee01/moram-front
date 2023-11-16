f71eb0bd408910c74ea6c29f3c4fb92841318eaa
nemokoala <fdxguy2@gmail.com>
2023-11-17 02:56:06 +0900

    fix: gpt 타임아웃 시간 증가

--- src/routes/SelfIntroduction.tsx
+++ src/routes/SelfIntroduction.tsx
@@ -47,7 +47,7 @@ function SelfIntroDuction() {
       const response = await axios.post(
         `${process.env.REACT_APP_APIADDRESS}/gpt`,
         { content: text },
-        { timeout: 500000 }
+        { timeout: 900000 }
       );
       setResult(response.data.content); //포스팅 데이터 받기
       setIsLoading(false);
